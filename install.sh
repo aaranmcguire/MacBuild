35fe46e2c78ed08caf9835c513b791fa90fb2585
Aaran McGuire <aaran.mcguire@cl.vc>
2017-03-16 18:39:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 
 # Install X Code Develoeper tools. This includes git.
-xcode-select --install
+xcode-select --install;
 
 # Install pip (Python's package manager). Installing to local user only to bypass System Integrity Protection.
-easy_install --user pip
+easy_install --user pip;
 
 # Update PATH for this session only.
-export PATH="$HOME/Library/Python/2.7/bin:$PATH"
+export PATH="$HOME/Library/Python/2.7/bin:$PATH";
 
 # Install ansible.
-pip install --user --upgrade ansible
+pip install --user --upgrade ansible;
 
 # Clone MacBuild Repo
-git clone https://github.com/aaranmcguire/MacBuild
+git clone https://github.com/aaranmcguire/MacBuild; cd MacBuild;
 
 # Run ansuble (-K option requests sudo password).
-ansible-playbook ./MacBuild/main.yml -K
+ansible-playbook main.yml -K;
